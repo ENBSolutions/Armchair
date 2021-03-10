5f8be32d1c0727ff411937b681eaa641f31f0bae
Ismail Elmaliki <ielmalik@meredith.com>
2021-03-10 11:24:44 -0500

    Support for swift package manager

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Armchair",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Armchair",
+            targets: ["Armchair"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Armchair",
+            path: "Source",
+            exclude: ["Info.plist", "Info-Localizable.plist"])
+    ]
+)
